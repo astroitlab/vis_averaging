2eb367b714fd7f0cb22c890f9fec5c17cbea7be9
kaidaluo <62178917+kaidaluo@users.noreply.github.com>
2020-05-25 13:35:54 +0800

    Update view_image.py

--- view_image.py
+++ view_image.py
@@ -1,4 +1,7 @@
 fieldlist=['1','2','4','8','16']
 for fd in fieldlist:
     imagename = fd + '_IRC+10216_img.image'
+    psfname = fd + '_IRC+10216_img.psf'
+    
     viewer(imagename)
+    viewer(psfname)
